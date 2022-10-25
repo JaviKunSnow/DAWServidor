18c8ce01240bb2ffdc767a29e5300be3aee3699f
JaviKunSnow <javierandres_11@hotmail.es>
2022-10-25 19:03:15 +0000

    9.0

---
+++ DWEC/Tema01/Ejercicio006/js/rombo.js
@@ -0,0 +1,37 @@
+
+function romboCambio(){
+    const valor = document.getElementById("input_filas").value;
+    const div = document.getElementById("divi");
+    if(div.innerHTML != ""){
+        div.innerHTML = "";
+    }
+
+    for(let i = 1; i <= valor; i++){
+        for(let blanco = 1; blanco <= valor-i; blanco++){
+            div.innerHTML += "&nbsp;&nbsp;";
+        }
+
+        for(let astericos=1; astericos <= (i*2) - 1; astericos++){
+            if(astericos == 1 || astericos == (i*2)-1){
+                div.innerHTML += "*";
+            } else {
+                div.innerHTML += "&nbsp;&nbsp;";
+            }
+        }
+        div.innerHTML += "<br/>";
+    }
+    for(let i = valor; i >= 1; i--){
+        for(let blanco = 1; blanco <= valor-i; blanco++){
+            div.innerHTML += "&nbsp;&nbsp;";
+        }
+
+        for(let astericos=1; astericos <= (i*2)-1; astericos++){
+            if(astericos == 1 || astericos == (i*2)-1){
+                div.innerHTML += "*";
+            } else {
+                div.innerHTML += "&nbsp;&nbsp;";
+            }
+        }
+        div.innerHTML += "<br/>";
+    }
+}
