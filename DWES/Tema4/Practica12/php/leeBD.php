849cd3ab61d67be3dd82e9c0f22e6d4efffd27e0
JaviKunSnow <javierandres_11@hotmail.es>
2022-12-07 15:43:06 +0000

    21.0

--- DWES/Tema4/Practica12/php/leeBD.php
+++ DWES/Tema4/Practica12/php/leeBD.php
@@ -13,37 +13,55 @@
         include_once("../../../cabecera.html");
     ?>
     <form action="./leeBD.php" method="post">
+        <table border='1'>
+            <tr>
+                <th>id</th>
+                <th>Jugador</th>
+                <th>Edad</th>
+                <th>PPP</th>
+                <th>APP</th>
+                <th>RPP</th>
+                <th>Fecha debut</th>
+                <th>Editar/Borrar</th>
+            </tr>
         <?php
-            $conexion = new mysqli();
-            $conexion->connect($_SERVER["SERVER_ADDR"],USER,PASS, BBDD);
-            echo "<table border='1'>";
-            echo "<tr>";
-                echo "<th>id</th>";
-                echo "<th>Jugador</th>";
-                echo "<th>Edad</th>";
-                echo "<th>PPP</th>";
-                echo "<th>APP</th>";
-                echo "<th>RPP</th>";
-                echo "<th>Fecha debut</th>";
-                echo "<th>Modificar</th>";
-                echo "<th>Borrar</th>";
-            echo "</tr>";
-            $sql = 'select * from LosAngelesLakers';
-            $resultado = mysqli_query($conexion, $sql);
-            $i = 0;
-            while($filas = mysqli_fetch_assoc($resultado)){
-                echo "<tr>";
-                echo "<td>".$fila["id"]."</td>";
-                echo "<td>".$fila["jugador"]."</td>";
-                echo "<td>".$fila["edad"]."</td>";
-                echo "<td>".$fila["puntos"]."</td>";
-                echo "<td>".$fila["asistencias"]."</td>";
-                echo "<td>".$fila["rebotes"]."</td>";
-                echo "<td>".$fila["fechadebut"]."</td>";
-                echo "<td><a href='./editaBD.php?numero=".$i++."'>Editar</a></td>";
-                echo "</tr>";
+            try {
+                $conexion = new mysqli();
+                $conexion->connect($_SERVER["SERVER_ADDR"],"javier",PASS, BBDD);
+                $sql = 'select * from LosAngelesLakers';
+                $resultado = mysqli_query($conexion, $sql);
+                $i = 0;
+                while($filas = mysqli_fetch_assoc($resultado)){
+                    echo "<tr>";
+                    echo "<td>".$fila["id"]."</td>";
+                    echo "<td>".$fila["jugador"]."</td>";
+                    echo "<td>".$fila["edad"]."</td>";
+                    echo "<td>".$fila["puntos"]."</td>";
+                    echo "<td>".$fila["asistencias"]."</td>";
+                    echo "<td>".$fila["rebotes"]."</td>";
+                    echo "<td>".$fila["fechadebut"]."</td>";
+                    echo "<td><a href='./editaBD.php?numero=".$i++."'>Editar/Borrar</a></td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+                $conexion->close();
+            }catch(Exception $ex) {
+                if($ex->getCode() == 1049){
+                    echo "<span style='color: red;'>La base de datos no existe.</span>";
+                    $conexion->close();
+                    ?>
+                        <input type="submit" name="enviar" value="crear BD">
+                    <?
+                }
+                if($ex->getCode() == 1045){
+                    echo "<span style='color: red;'>El nombre de usuario o la contraseña no es correcto.</span>";
+                    $conexion0->close();
+                }
+                if($ex->getCode() == 2002){
+                    echo "<span style='color: red;'>Se acabo el tiempo de espera, no hemos podido establecer la conexion.</span>";
+                    $conexion0->close();
+                }
             }
-            echo "</table>";
         ?>
     </form>
     <h2>Ver codigo:</h2>
