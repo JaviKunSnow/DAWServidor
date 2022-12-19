b18594e31aeb54935124a6d3cbfaf11d5b9ff30f
javier <javierandres_11@hotmail.es>
2022-12-19 09:15:03 +0000

    24.4

---
+++ DWES/Tema5/sessiones/funciones/BD.php
@@ -0,0 +1,39 @@
+<?php
+
+require("../seguro/conexion.php");
+
+function validaUser($user, $pass) {
+    try {
+        $conexion = new PDO("mysql:host=".$_SERVER["SERVER_ADDR"].";dbname=".BBDD, USER, PASS);
+        $sql = "select * from usuarios where usuario = ? and clave = ?";
+        $sql_p = $conexion->prepare($sql);
+        $pass_e = sha1($pass);
+        $array = array($user, $pass_e);
+        $sql_p->execute();
+
+        //si devuelve algo retorna true
+        if($sql_p->rowCount() == 1){
+            session_start();
+            $_SESSION['validado'] = true;
+            $row = $sql_p->fetch();
+            $_SESSION['user'] = $user;
+            $_SESSION['nombre'] = $row['nombre'];
+            $_SESSION['perfil'] = $row['perfil'];
+            unset($conexion);
+            return true;
+        } else {
+            // si no, no retorna falso
+            unset($conexion);
+            return false;
+        }
+
+
+
+    } catch (Exception $ex) {
+        print_r($ex);
+        unset($conexion);
+    }
+
+}
+
+?>
---
+++ DWES/Tema5/sessiones/funciones/valida.php
@@ -0,0 +1,21 @@
+<?php
+
+require("./BD.php");
+
+$user = $_REQUEST["user"];
+$pass = $_REQUEST["pass"];
+
+if(validaUser($user, $pass)){
+    if($_SESSION["perfil"] == "ADM1") {
+        header("location: ../paginas/admin.php");
+        exit();
+    } else {
+        header("location: ../paginas/home.php");
+        exit();
+    }
+} else {
+    header("location: ../login.php");
+    exit();
+}
+
+?>
