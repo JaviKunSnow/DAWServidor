8e1e067575e28ee6cedb1510967fbd77d6ae2bf5
javier <javierandres_11@hotmail.es>
2023-01-23 08:40:21 +0000

    27.3

--- DWES/Tema6/mvc/controlador/productoController.php
+++ DWES/Tema6/mvc/controlador/productoController.php
@@ -1,8 +1,13 @@
 <?
 if(isset($_REQUEST['cod_producto'])) {
     $_SESSION['producto'] = $_REQUEST['cod_producto'];
-    $producto = ProductoDAO::findById($_SESSION['cod_producto']);
+    $producto = ProductoDAO::findById($_SESSION['producto']);
+} else if(isset($_REQUEST['admProductos'])) {
+    $lista = ProductoDAO::findAll();
+} else {
+    $producto = ProductoDAO::findById($_SESSION['producto']);
 }
+
 
 
 ?>
