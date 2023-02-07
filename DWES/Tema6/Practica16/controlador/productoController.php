b1fa5162d13831101d20fcead1c8bae402af9e40
javier <javierandres_11@hotmail.es>
2023-02-07 17:23:06 +0000

    29.8

--- DWES/Tema6/Practica16/controlador/productoController.php
+++ DWES/Tema6/Practica16/controlador/productoController.php
@@ -1,20 +1,2 @@
-<?
-
-if(isset($_REQUEST['borrar'])) {
-    $_SESSION['producto'] = $_REQUEST['cod_producto'];
-    $producto = ProductoDAO::delete($_SESSION['producto']);
-    $lista = ProductoDAO::findAll();
-} else if(isset($_REQUEST['editar'])) {
-    $_SESSION['accionProducto'] = 'editar';
-    $_SESSION['producto'] = $_REQUEST['cod_producto'];
-    $producto = ProductoDAO::findById($_SESSION['producto']);
-    $_SESSION['vista'] = $vistas['editarProducto'];
-} else if(isset($_REQUEST['insertar'])) {
-    $_SESSION['accionProducto'] = 'insertar';
-    $_SESSION['vista'] = $vistas['editarProducto'];
-    require_once './vista/layout.php';
-} else {
-    $lista = ProductoDAO::findAll();
-}
 
 ?>
