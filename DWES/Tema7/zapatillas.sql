956533fa4bce4ed781bb5ccb90e097353454b1a9
javier <javierandres_11@hotmail.es>
2023-02-10 18:54:47 +0000

    30.3

---
+++ DWES/Tema7/zapatillas.sql
@@ -0,0 +1,50 @@
+create database zapatillas;
+use zapatillas;
+
+create table perfil (
+    codigo char(3) primary key,
+	descripcion varchar(30)
+) engine=innodb;
+
+create table usuarios (
+    usuario CHAR(40) primary key,
+    nombre VARCHAR(60) NOT NULL,
+    pass CHAR(200) NOT NULL,
+    correo varchar(75) NOT NULL,
+    fechanac date NOT NULL,
+    perfil CHAR(3) NOT NULL,
+    index (perfil),
+	foreign key (perfil) references perfil (codigo)
+) engine=innodb;
+
+create table producto (
+    cod_producto int primary key auto_increment,
+    nombre VARCHAR(40) NOT NULL,
+    descripcion VARCHAR(300) NOT NULL,
+    foto VARCHAR(40) NOT NULL,
+    precio float(6) NOT NULL,
+    stock int NOT NULL
+) engine=innodb;
+
+create table ventas (
+    id_venta int primary key auto_increment,
+    usuario CHAR(40) NOT NULL,
+    fechacomp date NOT NULL,
+    cod_producto int NOT NULL,
+    cantidad int NOT NULL,
+    precio_total float(6) NOT NULL,
+    foreign key (usuario) references usuarios (usuario),
+    foreign key (cod_producto) references producto (cod_producto)
+) engine=innodb;
+
+
+
+create table pedidos (
+    id_albaran int primary key auto_increment,
+    fechaalb date NOT NULL,
+    cod_producto int NOT NULL,
+    cantidad int NOT NULL,
+    usuario CHAR(40) NOT NULL,
+    foreign key (cod_producto) references producto (cod_producto),
+    foreign key (usuario) references usuarios (usuario)
+) engine=innodb;
