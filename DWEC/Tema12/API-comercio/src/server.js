28e86c46ac4caa11750db6a2050721ce0e917ebc
javier <javierandres_11@hotmail.es>
2023-02-13 13:15:14 +0000

    30.6

--- DWEC/Tema12/API-comercio/src/server.js
+++ DWEC/Tema12/API-comercio/src/server.js
@@ -1,7 +1,28 @@
 const express = require("express");
 const app = express();
 
+const mysql = require('mysql2');
+const myconn= require('express-myconnection');
+
+const rutas= require('./routes');
+
+const dbOptions= {
+    //propiedad host donde se ubica la base de datos Mysql
+    host: 'localhost',
+    //propiedad puerto donde escucha mysql (por defecto es 3306)
+    port: 3306,
+    //usuario base de datos (por defecto root)
+    user: 'javier',
+    //contraseña del usuario
+    password: 'javier',
+    //nombre del esquema al que nos vamos a conectar
+    database: 'comercio'
+}
+
 app.set("port", process.env.PORT || 9000);
+app.use(myconn(mysql, dbOptions, 'single'));
+app.use('/api', rutas);
+
 
 app.listen(app.get('port'), "192.168.2.206", ()=>{
     console.log("Servidor corriendo en puerto", app.get('port'));
